23dec2582f162cf2a162f449b217c3066a533381
Mikko Torvinen <mikko.torvinen82@gmail.com>
2021-06-11 13:22:59 +0300

    Add bit stuff to gatsby project

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 1000,
+  },
+});
+
+export default function AboutMe() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h2" component="h2" gutterBottom>
+        Mikko Torvinen
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+      Hi! Nice to see you at my profile. Here’s short introduction about computer nerd called Mikko Torvinen
+      </Typography>
+      <br/>
+      <Typography variant="body1" gutterBottom>
+      As said my name is Mikko Torvinen. I started my career with computers when I was in high school by doing some simple programming using Visual Basic and creating web pages with HTML. I have always been interested of computers and possibilities computers can bring our everyday life
+I continued my studies in Kemi-Tornio University of Applied Science with digital technology as major. Studies were included with design and implement embedded devices as well as designing digital filters. As minors studies I had courses in software development and working in software projects.
+      </Typography>
+      <br/>
+      <Typography variant="body1" gutterBottom>
+      My working career started on Nokia back in 2006. I was participating on software development organization responsible of audio adaption software in Symbian based mobile phones. My key responsibilities were develop automatic testing and building environments around mobile phones. Our main challenges were integrate several software sources into one reliability build automatically.
+      </Typography>
+      <br/>
+      <Typography variant="body1" gutterBottom>
+      Thou that I have always been interested of web applications and programming using web tools I joined Technia PLM Oy in year 2011. In Technia I worked as web application developer in area of Product Lifecycle Management. I was working on both front-end and back-end activities using tools such as Java, JavaScript, jQuery, Struts and Spring. In Technia our PLM environment was Enovia provided by Dassault Systemes.
+      </Typography>
+      <br/>
+      <Typography variant="body1" gutterBottom>
+      After good and educated years in Technia I had possibility to join another PLM company in Finland called IDEAL PLM. Compared to Technia IDEAL PLM is using Siemens PLM tools Teamcenter. Now I’m using my previous competence to strengthen IDEAL PLM marketing in area of Northern Finland as well as learning new ways to handle PLM.
+      </Typography> 
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,108 @@
-import * as React from "react"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Footer from './Footer';
+import AboutMe from './AboutMe';
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 500,
-}
+};
 const headingAccentStyles = {
   color: "#663399",
+};
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
-// data
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
 
-let countDownDate = new Date("Jun 18, 2021 16:00:00").getTime();
-// Update the count down every 1 second
-let x = setInterval(function() {
+function tabProperties(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
-  // Get today's date and time
-  let now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  let distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("counter").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("counter").innerHTML = "EXPIRED";
-  }
-}, 1000);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 // markup
 const IndexPage = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <main style={pageStyles}>
-      <title>Countdown</title>
-      <h1 style={headingStyles}>
-        This is my countdown
-        <br />
-        <span id="counter" style={headingAccentStyles}></span>
-      </h1>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="tabs"
+          >
+            <Tab label="About Me" {...tabProperties(0)} />
+            <Tab label="Hockey Stats" {...tabProperties(1)} />
+            <Tab label="Don't know yet" {...tabProperties(2)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <AboutMe/>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </div>
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
+      <Footer title="Mikko Torvinen" description="Playground powered by Gatsby, React and MaterialUI" />
     </main>
-  )
-
-}
-export default IndexPage
-
+  );
+};
+export default IndexPage;
