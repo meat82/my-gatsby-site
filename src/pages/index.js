551e52bd442aacdcfa60ef73bc7ea60faaecac10
Mikko Torvinen <mikko.torvinen82@gmail.com>
2021-06-14 11:06:47 +0300

    css practice

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import Footer from './Footer';
-import AboutMe from './AboutMe';
+import Footer from "./Footer";
+import AboutMe from "./AboutMe";
 
 // styles
 const pageStyles = {
@@ -57,39 +57,55 @@ function tabProperties(index) {
   };
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: "white",
   },
-}));
+});
+
+const appBarStyle = makeStyles({
+  root:{
+    color: "default"
+  },
+  colorPrimary: {
+    "background-color": "#20232f",
+  },
+  colorSecondary: {
+    "background-color": "black",
+  }
+});
 
 // markup
 const IndexPage = () => {
   const classes = useStyles();
+  const appBar = appBarStyle();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-  console.log("IndexPage: ")
-  console.log(classes)
+  console.log("IndexPage: ");
+  console.log(classes);
   return (
     <main style={pageStyles}>
-      <div className="makeStyles-root-19">
-        <AppBar position="static">
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="tabs"
-          >
+      <div className={classes.root}>
+        <AppBar
+          position="static"
+          classes={{
+            root: appBar.root,
+            colorPrimary: appBar.colorPrimary,
+            colorSecondary: appBar.colorSecondary
+          }}
+        >
+          <Tabs value={value} onChange={handleChange} aria-label="tabs">
             <Tab label="About Me" {...tabProperties(0)} />
             <Tab label="Hockey Stats" {...tabProperties(1)} />
             <Tab label="Don't know yet" {...tabProperties(2)} />
           </Tabs>
         </AppBar>
         <TabPanel value={value} index={0}>
-          <AboutMe/>
+          <AboutMe />
         </TabPanel>
         <TabPanel value={value} index={1}>
           Item Two
@@ -102,7 +118,10 @@ const IndexPage = () => {
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
-      <Footer title="Mikko Torvinen" description="Playground powered by Gatsby, React and MaterialUI" />
+      <Footer
+        title="Mikko Torvinen"
+        description="Playground powered by Gatsby, React and MaterialUI"
+      />
     </main>
   );
 };
