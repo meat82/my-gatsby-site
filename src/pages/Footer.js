bdf6f246bff7412d5abde7b461da1a58a01f7c14
Mikko Torvinen <mikko.torvinen82@gmail.com>
2021-06-15 08:33:32 +0300

    footer fix

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -20,9 +20,8 @@ function Copyright() {
 
 const useStyles = makeStyles((theme) => ({
   footer: {
-    backgroundColor: "theme.palette.background.paper",
-    // marginTop: theme.spacing(8),
     padding: theme.spacing(6, 0),
+    "background-color": "white"
   },
 }));
 
